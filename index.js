28a34633143b7e033127c5bc75f2fe04509aeda2
enti <enti@enti.enti>
2022-03-18 11:22:11 +0100

    Filters and projections

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let http = require("http");
 let fs = require("fs");
 
 let mongo_client = require("mongodb").MongoClient;
+let ObjectId = require("mongodb").ObjectID;
+
 
 let url = "mongodb://localhost/";
 
@@ -24,6 +26,28 @@ mongo_client.connect(url, function(error, conn){
 });
 
 
+function send_data_list (db, req, res)
+{
+	let col = "";
+
+	if (req.url == "/characters")
+		col = "characters";
+	else if (req.url == "/items")
+		col = "items";
+	else{
+		res.end();
+		return;
+	}
+
+	let col_data = db.collection(col).find({}, { projection: { name:1 } });
+
+	col_data.toArray(function(err, data){
+		let string = JSON.stringify(data);
+
+		res.end(string);
+	});
+}
+
 
 http.createServer(function(req, res){
 	res.writeHead(200);
@@ -37,26 +61,35 @@ http.createServer(function(req, res){
 		return;
 	}
 
-	let col = "";
 
-	if (req.url == "/characters")
-		col = "characters";
-	else if (req.url == "/items")
-		col = "items";
-	else{
+	let url = req.url.split("/");
+
+
+	if (url.length == 2){
+		send_data_list(db, req, res);
+
+		return;
+	}
+
+	if (url[2].length != 24){
 		res.end();
 		return;
 	}
 
-	let col_data = db.collection(col).find();
+	if (url[1] == "characters"){
+		let obj_id = new ObjectId(url[2]);
 
-	col_data.toArray(function(err, data){
-		let string = JSON.stringify(data);
+		let col_data = db.collection("characters").find({"_id":obj_id});
 
-		res.end(string);
-	});
+		col_data.toArray(function(err, data){
+			let string = JSON.stringify(data);
 
+			res.end(string);
+		});
+	}
+	else if (url[1] == "items") {
 
+	}
 }).listen(1095);
 
 
